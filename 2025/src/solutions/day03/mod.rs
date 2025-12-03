b7484d079fca7af28d9461c1497522cde8afc916
Marek Macznik <marek.macznik@gmail.com>
2025-12-03 19:11:13 +0100

    aoc2025, day3, part1

--- 2025/src/solutions/day03/mod.rs
+++ 2025/src/solutions/day03/mod.rs
@@ -5,8 +5,8 @@ impl Solver for Problem {
     type Ans1 = u64;
     type Ans2 = u64;
 
-    fn solution1(&self, _input: &str) -> Self::Ans1 {
-        0
+    fn solution1(&self, input: &str) -> Self::Ans1 {
+        input.lines().map(|line| biggest_in_sequence(line)).sum()
     }
 
     fn solution2(&self, _input: &str) -> Self::Ans2 {
@@ -14,15 +14,49 @@ impl Solver for Problem {
     }
 }
 
+fn biggest_in_sequence(seq: &str) -> u64 {
+    let mut max_suffix: Option<u8> = None;
+    let mut best: Option<u8> = None;
+
+    for ch in seq.chars().rev() {
+        let d = ch.to_digit(10).unwrap() as u8;
+
+        match max_suffix {
+            None => {
+                max_suffix = Some(d);
+            }
+            Some(suf) => {
+                let value = d * 10 + suf;
+                if best.map_or(true, |b| value > b) {
+                    best = Some(value);
+                }
+
+                if d > suf {
+                    max_suffix = Some(d);
+                }
+            }
+        }
+    }
+
+    best.map(|v| format!("{:02}", v)).unwrap().parse::<u64>().unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use crate::solutions::day03::*;
 
     #[test]
+    fn test_biggest_pair_in_sequence() {
+        let seq: &str = "976543211111118";
+        let ans: u64 = biggest_in_sequence(seq);
+        assert_eq!(ans, 98);
+    }
+
+    #[test]
     fn test_solution1() {
         let problem = Problem {};
         let ans = problem.solution1(TEST_INPUT_1);
-        assert_eq!(ans, 0);
+        assert_eq!(ans, 357);
     }
 
     #[test]
@@ -32,5 +66,8 @@ mod tests {
         assert_eq!(ans, 0);
     }
 
-    const TEST_INPUT_1: &str = "";
+    const TEST_INPUT_1: &str = "987654321111111
+811111111111119
+234234234234278
+818181911112111";
 }
